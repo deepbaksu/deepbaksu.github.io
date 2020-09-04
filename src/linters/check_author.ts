83bcc83bda681234681fcec62374256d76e8b2c2
Mo Kweon <kkweon@google.com>
2020-09-03 17:35:06 -0700

    Add a markdown linter for the repo.
    Initially, it will begin with a author: check

---
+++ src/linters/check_author.ts
@@ -0,0 +1,18 @@
+import { MarkdownLinter } from "./types";
+
+export const checkAuthor: MarkdownLinter = (
+  filename: string,
+  content: string
+) => {
+  if (/\nauthor:\s/.test(content)) {
+    return null;
+  }
+  return new Error(`author: tag is required in ${filename}.
+
+For example, add the following in ${filename}.
+
+---
+author: Mo Kweon
+---
+`);
+};
