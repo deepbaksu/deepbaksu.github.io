9cff0648a14c1e4c6cbd9a4a0a348ad4c018089c
Mo Kweon <kkweon@google.com>
2020-09-14 11:28:29 -0700

    Add image auth tag check

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,9 @@ import { promises as fs } from "fs";
 import { MarkdownLinter } from "./linters/types";
 import { checkAuthor } from "./linters/check_author";
 import process from "process";
+import { checkImageAlt } from "./linters/check_image_should_have_alt";
 
-const markdownLinters: MarkdownLinter[] = [checkAuthor];
+const markdownLinters: MarkdownLinter[] = [checkAuthor, checkImageAlt];
 
 glob("source/_posts/**/*.md", (err, filenames) => {
   if (err) {
@@ -18,6 +19,7 @@ glob("source/_posts/**/*.md", (err, filenames) => {
         return fh.readFile("utf-8");
       })
       .then(buffer => {
+        console.log(`Checking ${filename}`);
         for (const linter of markdownLinters) {
           const err = linter(filename, buffer);
 
@@ -29,6 +31,9 @@ glob("source/_posts/**/*.md", (err, filenames) => {
       .catch(err => {
         console.error(err);
         process.exit(1);
+      })
+      .then(() => {
+        console.log(`Done checking ${filename}`);
       });
   }
 });
